fd15a6d0e68931dc83d8a9f313c7773ecc7891c0
NikiBober <95879723+NikiBober@users.noreply.github.com>
2024-08-05 17:39:52 +0300

    0.0 Project imported. Start.

---
+++ Assets/Scripts/HeroScreen/HeroProgressData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class HeroProgressData
+{
+    public string Name;
+    public Sprite Avatar;
+    public int Level;
+    public int LevelXpCurrent;
+    public int LevelXpMax;
+    public bool Unlocked;
+}
---
+++ Assets/Scripts/HeroScreen/HeroScreen.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class HeroScreen : MonoBehaviour
+{
+    public void Init(HeroProgressData[] heroes)
+    {
+        // TODO
+        /*
+         *  This method receives a list of heroes data objects of any amount
+         *  Fill the scroll rect with Tiles in amount according to received data objects 
+         *  If character is locked - show placeholder tile as it is the last one on example scene
+         *  Ignore the top label with "Heroes 6/8" 
+         */
+    }
+}
