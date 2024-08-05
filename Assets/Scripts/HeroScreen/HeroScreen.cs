679e9754527e7fcdeae57f64f3040c30856a0b83
NikiBober <95879723+NikiBober@users.noreply.github.com>
2024-08-05 18:12:10 +0300

    0.1 code
    ~ 1hr.

--- Assets/Scripts/HeroScreen/HeroScreen.cs
+++ Assets/Scripts/HeroScreen/HeroScreen.cs
@@ -2,8 +2,42 @@ using UnityEngine;
 
 public class HeroScreen : MonoBehaviour
 {
+    [Tooltip("Prefab for hero tile")]
+    [SerializeField] private GameObject _heroTilePrefab;
+
+    [Tooltip("Prefab for placeholder tile")]
+    [SerializeField] private GameObject _placeholderTilePrefab;
+
+    [Tooltip("Content container for the Scroll Rect")]
+    [SerializeField] private Transform _content;
+
     public void Init(HeroProgressData[] heroes)
     {
+        // Clear all existing children from the content
+        foreach (Transform child in _content)
+        {
+            Destroy(child.gameObject);
+        }
+
+        // Create tiles for each hero in the array
+        foreach (var hero in heroes)
+        {
+            if (hero.Unlocked)
+            {
+                // If the hero is unlocked, create a regular hero tile
+                GameObject tile = Instantiate(_heroTilePrefab, _content);
+
+                // Set hero data on the tile
+                HeroTile heroTile = tile.GetComponent<HeroTile>();
+                heroTile.SetHeroData(hero);
+            }
+            else
+            {
+                // If the hero is locked, create a placeholder tile
+                Instantiate(_placeholderTilePrefab, _content);
+            }
+        }
+
         // TODO
         /*
          *  This method receives a list of heroes data objects of any amount
---
+++ Assets/Scripts/HeroScreen/HeroTile.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HeroTile : MonoBehaviour
+{
+    [Tooltip("Text field for the hero's name")]
+    [SerializeField] private TextMeshProUGUI _nameText;
+
+    [Tooltip("Image for the hero's avatar")]
+    [SerializeField] private Image _avatarImage;
+
+    [Tooltip("Text field for the hero's level")]
+    [SerializeField] private TextMeshProUGUI _levelText;
+
+    [Tooltip("Text field for the hero's XP")]
+    [SerializeField] private TextMeshProUGUI _xpText;
+
+    [Tooltip("Slider for the hero's XP progress")]
+    [SerializeField] private Slider _xpSlider;
+
+    // Set the hero data on the tile
+    public void SetHeroData(HeroProgressData hero)
+    {
+        _nameText.text = hero.Name;
+        _avatarImage.sprite = hero.Avatar;
+        _levelText.text = $"Level: {hero.Level}";
+        _xpText.text = $"{hero.LevelXpCurrent}/{hero.LevelXpMax}";
+        _xpSlider.maxValue = hero.LevelXpMax;
+        _xpSlider.value = hero.LevelXpCurrent;
+    }
+}
